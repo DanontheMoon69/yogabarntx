22e9c0db488eaeb63cb6c2f957f7d7e60ad4cacd
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-02-26 18:24:24 -0600

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -100,8 +100,8 @@ const HeroBanner = ({ props }) => {
             {/* BLESSINGS OF SPRING EQUINOX */}
             <div className="items-center justify-center bg-white rounded-xl  ml-2 mt-3 py-2 px-4 shadow-md">
               <div className="flex justify-center text-md font-bold">
-               {/* <Image src={sun} width="30" height="30" className="mr-2" />{" "} */}
-                Blessing of Spring Equinox
+               
+                Blessings of Spring Equinox
               </div>
               <div className="flex justify-center text-sm font-bold text-center text-gray-500 ">
                 March 25th @ Yoga Barn, Whitesboro, Tx
