316485f20cb2928bf3c64881ac28a237930bb4e9
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-02-26 23:23:51 -0600

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
         <title>Yoga Barn - Whitesboro, Tx</title>
     <meta
               name="description"
-          content="Yoga Studio offering In Person and Virtual Yoga with a Vinyasa Flow base in Whitesboro Texas."
+          content="Yoga Studio offering In Person and Virtual Yoga with a Vinyasa Flow base in Whitesboro Texas. We also offer Yoga Workshops in studio and at local businesses"
           key="desc" />
         <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
       </Head>
