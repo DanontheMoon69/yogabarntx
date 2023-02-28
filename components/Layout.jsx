5cee232e56ca28cb77ecf7a89db6922390c95bea
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-02-27 21:48:22 -0600

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
           content="Yoga Studio offering In Person and Virtual Yoga with a Vinyasa Flow base in Whitesboro Texas. We also offer Yoga Workshops in studio and at local businesses"
           key="desc"
         />
-        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="/image/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -23,13 +23,13 @@ const Layout = ({ children }) => {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/images/favicon-32x32.png"
+          href="/image/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/images/favicon-16x16.png"
+          href="/image/favicon-16x16.png"
         />
         <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
       </Head>
