9bf0e96099b4a060f60a6eea1e9219be95f1294b
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-03-01 16:59:52 -0600

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -69,7 +69,7 @@ const HeroBanner = ({ props }) => {
             <div className="items-center justify-center bg-white rounded-xl  ml-2 mt-3 py-2 px-4 shadow-md">
               <div className="flex justify-center items-center text-md font-bold">
                 <Image src={coffee} width="30" height="30" className="mr-2" />
-                Fill Your Cup @ Combs&apos Coffee;
+                Fill Your Cup @ Combs&apos; Coffee
               </div>
               <div className="flex justify-center text-sm font-bold text-center text-gray-500 ">
                 March 18th @ Combs&apos; Coffee, Gainesville, Tx
