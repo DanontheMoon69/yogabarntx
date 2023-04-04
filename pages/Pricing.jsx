5e30b709b78eb05d323d0a9b522ae26ff86f26e7
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-04-04 09:42:29 -0500

    Update Pricing.jsx

--- pages/Pricing.jsx
+++ pages/Pricing.jsx
@@ -299,7 +299,7 @@ function PricingPage() {
               <div className="flex p-2 items-center text-2xl font-thin">
                 <div> $110</div>
                 {user && (
-                  <Link href="https://buy.stripe.com/00gdUdcjaekq15C14l">
+                  <Link href="https://buy.stripe.com/3cs6rLcja9067u000f">
                     <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8 ml-4 text-white rounded-full">
                       Pay
                     </button>
