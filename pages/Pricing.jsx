bfb5dc58608ecd973121696d2c6a2fa4a3298cae
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-04-15 09:11:38 -0500

    Add files via upload

--- pages/Pricing.jsx
+++ pages/Pricing.jsx
@@ -48,7 +48,7 @@ function PricingPage() {
   text-white text-center w-full flex flex-col bottom-3"
         >
           <h2 className=" text-3xl font-thin tracking-wide text-white ">
-            Virtual yoga coming soon!
+            Virtual Yoga Now Available!
           </h2>
         </div>
       </div>
@@ -416,7 +416,7 @@ function PricingPage() {
                 </div>
                 Enlightened Family Pack
               </div>
-              <p className="text-xs font-normal bg-gray-100 py-2 px-2 rounded-xl">
+              <p className="text-xs font-thin tracking-wider bg-gray-100 py-2 px-2 rounded-xl">
                 Bring a friend or family member with you to enjoy your monthly
                 unlimited classes with. - Includes 2 Members and expires after
                 30 days from first attended class. Each Additional member is $40
@@ -448,6 +448,43 @@ function PricingPage() {
                     </span>
                   </Link>
                 </div>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+          {/* VIRTUAL YOGA */}
+          <div className="items-center justify-center  border-2 border-gray-100 shadow-md md:max-w-sm sm:w-1/2 rounded-lg mt-2 bg-gray-200 ">
+            <div className="p-2 text-lg font-thin tracking-wide text-black">
+              <div className="flex items-center mb-1">
+                {" "}
+                <div className="flex justify-end ml-3 mx-3 max-w-1/2   ">
+                  <Image className="h-6 w-10" src={ybLogo} alt="Yoga Barn" />
+                </div>
+                Virtual Yoga
+                <div className="flex ml-3 py-1 px-4  text-center justify-center font-bold  items-center bg-teal-700 text-white    rounded-full tracking-wide   text-xs animate-pulse ">
+                  NEW PACK
+                </div>
+              </div>
+              <p className="text-xs font-thin tracking-wider bg-gray-100 py-2 px-2 rounded-xl">
+                Monthly Unlimited Virtual Yoga with Auto-Renewal
+              </p>
+              <div className="flex p-2 items-center text-2xl font-thin">
+                <div> $40</div>
+                {user && (
+                  <Link href="https://buy.stripe.com/3cs6rLcja9067u000f">
+                    <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8 ml-4 text-white rounded-full">
+                      Pay
+                    </button>
+                  </Link>
+                )}
+                {!user && (
+                  <Link href="/signUpPage">
+                    <button className="ease-in transform hover:scale-105 transition duration-100 text-xs bg-teal-600 py-1 px-4 ml-4 text-white rounded-full">
+                      Sign-Up to Purchase
+                    </button>
+                  </Link>
+                )}
+                <div className="flex-wrap ml-2 justify-center ease-in transform hover:scale-105 transition duration-100"></div>
               </div>
             </div>
           </div>
@@ -563,6 +600,42 @@ function PricingPage() {
                     </span>
                   </Link>
                 </div>
+              </div>
+            </div>
+          </div>
+          {/* VIRTUAL YOGA */}
+          <div className="items-center justify-center  border-2 border-gray-100 shadow-md md:max-w-sm sm:w-1/2 rounded-lg mt-2 bg-gray-200 ">
+            <div className="p-2 text-lg font-thin tracking-wide text-black">
+              <div className="flex items-center mb-1">
+                {" "}
+                <div className="flex justify-end ml-3 mx-3 max-w-1/2   ">
+                  <Image className="h-6 w-10" src={ybLogo} alt="Yoga Barn" />
+                </div>
+                Virtual Yoga
+                <div className="flex ml-3 py-1 px-4  text-center justify-center font-bold  items-center bg-teal-700 text-white    rounded-full tracking-wide   text-xs animate-pulse ">
+                  NEW PACK
+                </div>
+              </div>
+              <p className="text-xs font-thin tracking-wider bg-gray-100 py-2 px-2 rounded-xl">
+                Monthly Unlimited Virtual Yoga with No Commitment
+              </p>
+              <div className="flex p-2 items-center text-2xl font-thin">
+                <div> $50</div>
+                {user && (
+                  <Link href="https://buy.stripe.com/3cs6rLcja9067u000f">
+                    <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8 ml-4 text-white rounded-full">
+                      Pay
+                    </button>
+                  </Link>
+                )}
+                {!user && (
+                  <Link href="/signUpPage">
+                    <button className="ease-in transform hover:scale-105 transition duration-100 text-xs bg-teal-600 py-1 px-4 ml-4 text-white rounded-full">
+                      Sign-Up to Purchase
+                    </button>
+                  </Link>
+                )}
+                <div className="flex-wrap ml-2 justify-center ease-in transform hover:scale-105 transition duration-100"></div>
               </div>
             </div>
           </div>
---
+++ pages/VirtualYoga.jsx
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import YouTube from "react-youtube";
+import { useStateContext } from "../context/StateContext";
+import virtualImage from "../components/assets/images/virtualYoga.jpeg";
+import Image from "next/image";
+
+import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
+
+const VirtualYoga = () => {
+  const [isVerified, setIsVerified] = useState(false);
+  const checkPw = () => {
+    // gets the current input value
+    const answer = document.getElementById("password").value;
+    const virtualPassword = process.env.NEXT_PUBLIC_VIRTUALPASS;
+
+    if (answer === virtualPassword) {
+      setIsVerified(true);
+    } else if (answer === "") {
+      alert("Please Enter an Answer");
+    } else {
+      alert("Incorrect Password");
+    }
+  };
+
+  return (
+    <>
+      {isVerified ? (
+        <>
+          <div className=" h-full pb-24">
+            <div className="relative text-center pt-24 font-thin text-2xl tracking-wider text-black">
+              <h1> YOGA BARN - VIRTUAL YOGA</h1>
+              <div className="relative w-full pt-4">
+                <Image
+                  src={virtualImage}
+                  alt="Yoga Barn"
+                  className=" object-cover h-48 "
+                />
+              </div>
+            </div>
+            <div className="text-white py-8 px-4 font-thin tracking-wider text-center bg-teal-400">
+              Below you'll find videos listed by Length to give you the option
+              to do a short class or a longer class.
+            </div>{" "}
+            {/*  */}
+            {/* 10 MINUTE VIDEOS */}
+            <h2 className=" text-3xl text-black font-thin tracking-wider flex justify-center pt-10">
+              10 Minute Yoga Classes
+            </h2>
+            {/*  */}
+            {/* 10 MINUTE VIDEOS CONTAINER */}
+            <div className="flex flex-wrap justify-center gap-3 pt-2">
+              {/*  */}
+              {/* 10 MINUTE - BALANCE */}
+              <div className="flex flex-col bg-teal-600 justify-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3 sm:rounded-lg">
+                <h3 className=" text-xl flex justify-center font-thin tracking-widest text-white text-center">
+                  Level: All Levels
+                  <br />
+                  Practice: Balance
+                </h3>
+                <YouTube
+                  videoId="sTnm5jvjgjM"
+                  autoplay="1"
+                  className="flex justify-center  p-2"
+                />
+              </div>
+              {/* END 10 MINUTE STRETCHING */}
+              {/*  */}
+              {/*  */}
+              {/* 10 MINUTE - Stretching */}
+              <div className="flex flex-col bg-teal-600 justify-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3 sm:rounded-lg">
+                <h3 className=" text-xl flex justify-center font-thin tracking-widest text-white text-center">
+                  Level: All Levels
+                  <br />
+                  Practice: Stretching
+                </h3>
+                <YouTube
+                  videoId="sTnm5jvjgjM"
+                  autoplay="1"
+                  className="flex justify-center  p-2"
+                />
+              </div>
+              {/* END 10 MINUTE STRETCHING */}
+              {/*  */}
+            </div>
+            {/*  */}
+            {/* END 10 MINUTE VIDEOS CONTAINER */}
+            {/*  */}
+            {/* 15 MINUTE VIDEOS */}
+            <h2 className=" text-3xl text-black font-thin tracking-wider flex justify-center pt-8">
+              15 Minute Yoga Classes
+            </h2>
+            {/*  */}
+            {/* 15 MINUTE VIDEOS CONTAINER */}
+            <div className="flex flex-wrap justify-center gap-3 pt-2">
+              {/*  */}
+              {/* 15 MINUTE - BALANCE */}
+              <div className="flex flex-col bg-teal-600 justify-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3 sm:rounded-lg">
+                <h3 className=" text-xl flex justify-center font-thin tracking-widest text-white text-center">
+                  Level: All Levels
+                  <br />
+                  Practice: Balance
+                </h3>
+                <YouTube
+                  videoId="sTnm5jvjgjM"
+                  autoplay="1"
+                  className="flex justify-center  p-2"
+                />
+              </div>
+              {/* END 15 MINUTE STRETCHING */}
+              {/*  */}
+              {/*  */}
+              {/* 15 MINUTE - Stretching */}
+              <div className="flex flex-col bg-teal-600 justify-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3 sm:rounded-lg">
+                <h3 className=" text-xl flex justify-center font-thin tracking-widest text-white text-center">
+                  Level: All Levels
+                  <br />
+                  Practice: Stretching
+                </h3>
+                <YouTube
+                  videoId="sTnm5jvjgjM"
+                  autoplay="1"
+                  className="flex justify-center  p-2"
+                />
+              </div>
+              {/* END 15 MINUTE STRETCHING */}
+              {/*  */}
+            </div>
+            {/*  */}
+            {/* END 15 MINUTE VIDEOS CONTAINER */}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col h-screen">
+            <div className="relative text-center pt-24 font-thin text-2xl tracking-wider text-black">
+              <h1> YOGA BARN - VIRTUAL YOGA</h1>
+              <div className="relative w-full pt-4">
+                <Image
+                  src={virtualImage}
+                  alt="Yoga Barn"
+                  className=" object-cover h-48 "
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center text-center mt-12">
+              <p className=" text-black font-thin tracking-wider">
+                This Page is Password Protected.
+              </p>
+              <p className=" text-black font-thin tracking-wider">
+                Virtual Yoga is included with our Bliss Pack or can be purchased
+                separately on our{" "}
+                <Link href="/Pricing">
+                  <span className="text-teal-600">Pricing Page</span>
+                </Link>
+                .
+              </p>
+              <div className=" text-black font-thin tracking-wider py-4">
+                Please Enter Password below
+              </div>
+
+              <div className="w-full flex justify-center items-center">
+                <form
+                  onSubmit={checkPw}
+                  className="flex flex-col justify-center items-center "
+                >
+                  <input
+                    className="flex justify-center h-8 bg-white border-2 border-teal-500"
+                    id="password"
+                    name="password"
+                  />
+                  <button className="flex justify-center items-center mt-2 font-thin tracking-wide bg-teal-500 rounded-full px-4 py-2 w-fit text-white">
+                    ENTER
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default VirtualYoga;
