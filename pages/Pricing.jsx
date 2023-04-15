22a0f5e33b6f9251a69fa1eee2d6002e15e419ab
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-04-15 09:26:09 -0500

    Add files via upload

--- pages/Pricing.jsx
+++ pages/Pricing.jsx
@@ -471,7 +471,7 @@ function PricingPage() {
               <div className="flex p-2 items-center text-2xl font-thin">
                 <div> $40</div>
                 {user && (
-                  <Link href="https://buy.stripe.com/3cs6rLcja9067u000f">
+                  <Link href="https://buy.stripe.com/5kA7vPcjab8e8y49AZ">
                     <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8 ml-4 text-white rounded-full">
                       Pay
                     </button>
@@ -622,7 +622,7 @@ function PricingPage() {
               <div className="flex p-2 items-center text-2xl font-thin">
                 <div> $50</div>
                 {user && (
-                  <Link href="https://buy.stripe.com/3cs6rLcja9067u000f">
+                  <Link href="https://buy.stripe.com/4gw17r96Yb8e5lS00o">
                     <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8 ml-4 text-white rounded-full">
                       Pay
                     </button>
