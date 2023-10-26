49124b2de30a6ff08ae3f41a95041800720eabb8
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2023-10-25 21:13:27 -0500

    Add files via upload

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineLeft, AiOutlineClose } from "react-icons/ai";
 import monthCal from "components/assets/images/October2023_wBackground.png";
 
 import { useStateContext } from "../context/StateContext";
@@ -14,7 +14,7 @@ const Schedule = () => {
     <>
       <div className="flex justify-center w-full  bg-gray-50 pt-24 pb-4 px-2">
         <div className="bg-white  sm:w-3/4 mt-10 px-2 py-2 opacity-100 rounded-xl">
-          <div className="flex mb-3">
+          <div className="flex justify-between mb-3">
             <button
               type="button"
               className="flex font-semibold text-sm text-white items-center animate-pulse bg-teal-600 rounded-full px-4 py-1"
@@ -24,6 +24,16 @@ const Schedule = () => {
             >
               <AiOutlineLeft /> Close Calendar
               <span className="text-white font-bold ml-1"></span>
+            </button>
+            <button
+              type="button"
+              className="flex font-semibold text-md  items-center animate-pulse  px-4 py-1"
+              onClick={() => {
+                setShowSchedule(false);
+              }}
+            >
+              <AiOutlineClose />
+              <span className="text-emerald-600 font-bold "></span>
             </button>
           </div>
           <Image src={monthCal} alt="Yoga Barn - May 2023 Schedule" />
