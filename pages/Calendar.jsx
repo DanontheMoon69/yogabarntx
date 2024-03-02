82aa1b8a8527ffb59f1c397afc0ebe462fdef98d
DanontheMoon69 <98707529+DanontheMoon69@users.noreply.github.com>
2024-03-01 22:53:57 -0600

    Add files via upload

--- pages/Calendar.jsx
+++ pages/Calendar.jsx
@@ -76,18 +76,56 @@ export default function Calendar() {
                 Upcoming Workshops
               </h2>
 
-              {/* YOGA PLAYSHOP - @ Studio */}
+              {/* SPRING EQUINOX - 108 SUN SALUTES */}
 
               <div>
                 <h3 className="text-3xl font-thin tracking-wider text-teal-600 mt-4">
-                  Saturday February 24th
+                  Saturday March 23rd
+                </h3>
+                <p className="text-2xl font-thin text-black mt-4">
+                  Spring Equinox - 108 Sun Salutes
+                </p>
+                <p className="flex text-sm font-thin text-black  tracking-wide">
+                  {" "}
+                  9:30am - 11:30am
+                </p>
+                <p className="text-xs font-semibold mt-2 text-gray-500">
+                  Class Description:
+                </p>
+                <p className="text-sm mt-1 text-gray-500  pb-2">
+                  We will be moving through 108 Sun Salutations to welcome this
+                  new season. This is a very challenging practice that help
+                  strengthen body and mind. Leave feeling refreshed and ready
+                  for what ever life offers you in this new season.
+                </p>
+                {user && (
+                  <Link href="https://buy.stripe.com/28o7vPbf60tA6pWaER">
+                    <button className="ease-in transform hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8  text-white rounded-full">
+                      Buy Ticket
+                    </button>
+                  </Link>
+                )}
+                {!user && (
+                  <Link href="/signUpPage">
+                    <button className=" ease-in transform font-bold hover:scale-105 transition duration-100 text-sm bg-teal-600 py-2 px-8  text-white rounded-full">
+                      Sign-Up to Purchase
+                    </button>
+                  </Link>
+                )}
+              </div>
+
+              {/* Full Moon Workshop */}
+
+              <div>
+                <h3 className="text-3xl font-thin tracking-wider text-teal-600 mt-4">
+                  Monday March 25th
                 </h3>
                 <p className="text-2xl font-thin text-black mt-4">
                   Full Moon Workshop
                 </p>
                 <p className="flex text-sm font-thin text-black  tracking-wide">
                   {" "}
-                  9:00am - 10:30am
+                  9:30am - 11:30am
                 </p>
                 <p className="text-xs font-semibold mt-2 text-gray-500">
                   Class Description:
